ce368bc92030d1adbcc7a459dafb4446d4c6eaa2
likhithar2507 <likhithagowda2507@gmail.com>
2022-04-04 12:26:01 +0530

    empflask

---
+++ App.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')
+def welcome():
+    return "WELCOME TO MY WEBSITE"
+
+@app.route('/add')
+def add():
+    return render_template("add.html")
+
+
+@app.route('/search')
+def search():
+    return render_template("search.html")
+
+
+@app.route('/update')
+def Update():
+    return render_template("update.html")
+
+
+@app.route('/delete')
+def delete():
+    return render_template ("delete.html")
+
+
+if __name__ == "__main__":
+    app.run()
